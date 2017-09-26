a791fa8fa71938708d9b1060bf6cb66effabb3c0
davigr <romanbaital@gmail.com>
2017-09-27 11:00:27 +1300

    Initial Commit

---
+++ ClusterWS-Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClusterWS-Swift
+//
+//  Created by Roman Baitaliuk on 27/09/17.
+//  Copyright © 2017 ByteKit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
